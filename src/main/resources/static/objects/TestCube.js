a391fa9cdac422ffb55901a2c14698d07883fe20
SnowPiece <54859642+SnowPiece@users.noreply.github.com>
2020-10-01 17:09:34 +0200

    a

--- src/main/resources/static/objects/TestCube.js
+++ src/main/resources/static/objects/TestCube.js
@@ -1,11 +1,14 @@
-let loader = new THREE.GLTFLoader();
 function createTestCube() {
 
-// dit is een comment
-    loader.load('textures/model.gltf', function(gltf){
-    let cube = gltf.scene;
+    let cube;
+
+    let loader = new THREE.GLTFLoader();
+
+    loader.load('textures/model.gltf', function(gltf) {
+    cube = gltf.scene;
+    });
+
     return cube;
-     });
 }
 
 export {createTestCube};
